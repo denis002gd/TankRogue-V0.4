e1785d44c3e4e2ef182d0859f879f31d7fa7e867
Denis <133595385+denis002gd@users.noreply.github.com>
2024-01-18 08:48:49 +0200

    Add files via upload

--- Assets/Scripts/BossScripts/TankBotScript.cs
+++ Assets/Scripts/BossScripts/TankBotScript.cs
@@ -15,6 +15,7 @@ public class TankBotScript : MonoBehaviour
     [SerializeField] public Transform player;
     [SerializeField] public Transform TankObject;
     [SerializeField] public Transform explosionPos;
+    [SerializeField] public GameObject Slider;
     private UnityEngine.AI.NavMeshAgent TankBot;
     private Vector3 fixPosition;
     [Header("Stats")]
@@ -44,6 +45,8 @@ public class TankBotScript : MonoBehaviour
         StartCoroutine(UpdatePath());
         HealthBar.maxValue = BossHealth;
         nextShootTime = Time.time;
+        Slider.SetActive(true);
+      
     }
 
     void Update()
